2641ab9341c963ea253a5382779cfe51c330e51e
sahil <sahilkholia9@gmail.com>
2024-02-22 17:54:23 +0530

    Initial setup

---
+++ ecoharbor-backend/src/main/java/com/ecoharbor/ecoharborbackend/EcoharborBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ecoharbor.ecoharborbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoharborBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoharborBackendApplication.class, args);
+	}
+
+}
